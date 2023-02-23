d958963376345fb0852770e711ba05d71a6c0220
David Blbulyan <blbulyandavbulyan@gmail.com>
2023-02-23 21:47:46 +0400

    task0 with square numbers solved

---
+++ Seminar1/task0/Program.cs
@@ -0,0 +1,6 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.Write("Введите число: ");
+string userAnswer = Console.ReadLine();
+int number = Convert.ToInt32(userAnswer);
+int result = number * number;
+Console.WriteLine("Квадрат числа: " + result);
