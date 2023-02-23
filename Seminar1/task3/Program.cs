fe6247be4fcfd77decf2761d7b098a8796036e30
David Blbulyan <blbulyandavbulyan@gmail.com>
2023-02-23 22:11:44 +0400

    task3 sucessfuly solved

---
+++ Seminar1/task3/Program.cs
@@ -0,0 +1,10 @@
+﻿// Задача №3. Напишите программу, которая будет выдавать название дня недели по заданному номеру.
+// 3 -> Среда 
+// 5 -> Пятница
+string []days = {"Понедельник", "Вторник", "Среда", "Четверг", "Пятница", "Суббота", "Воскресенье"};
+Console.Write("Введите номер дня: ");
+int numberOfDay = Convert.ToInt32(Console.ReadLine());
+if(numberOfDay > 7 | numberOfDay < 1){
+  Console.WriteLine("Вы ввели неправильный номер дня!");
+}
+else Console.WriteLine(days[numberOfDay - 1]);
