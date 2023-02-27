c66575ba164ff83c5fd887536ba3f82132a2db03
David Blbulyan <blbulyandavbulyan@gmail.com>
2023-02-27 22:01:05 +0400

    task11 solved

---
+++ Seminar2/task11/Program.cs
@@ -0,0 +1,7 @@
+﻿// Напишите программу, которая выводит случайное трехзначное число и удаляет вторую цифру этого числа.
+int random_number = Random.Shared.Next(100, 1000);
+Console.WriteLine(random_number);
+int first_digit = random_number / 100; // первая цифра трёхзначного числа
+int last_digit = random_number % 10;// последняя цифра трёхзначного числа
+int result = first_digit * 10 + last_digit;// формула разложения двухзначного числа по разрядам
+Console.WriteLine(result);
