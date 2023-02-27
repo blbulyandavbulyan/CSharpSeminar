52f90d9cb0575d3f587408fd21f917af6cff7eb0
David Blbulyan <blbulyandavbulyan@gmail.com>
2023-02-27 21:22:40 +0400

    demo task solved

---
+++ Seminar2/demo_task/Program.cs
@@ -0,0 +1,8 @@
+﻿// See https://aka.ms/new-console-template for more information
+Random random = new Random();
+int number = random.Next(10, 100);
+Console.WriteLine(number);
+int digit1 = number % 10;
+int digit2 = number / 10;
+int maximal_digit = Math.Max(digit1, digit2);
+Console.WriteLine(maximal_digit);
