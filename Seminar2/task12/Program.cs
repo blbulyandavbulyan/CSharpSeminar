3f8678fd1d87320d13a1189c0e198a7dc3c7030e
David Blbulyan <blbulyandavbulyan@gmail.com>
2023-02-27 22:14:45 +0400

    task12 solved

---
+++ Seminar2/task12/Program.cs
@@ -0,0 +1,10 @@
+﻿
+int a = Convert.ToInt32(Console.ReadLine());
+int b = Convert.ToInt32(Console.ReadLine());
+int c = a % b;
+if(c == 0){
+  Console.WriteLine("кратно");
+}
+else {
+  Console.WriteLine("не кратно, остаток " + c);
+}
