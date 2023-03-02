8966e7cb03d3d51e2d2066fc4f96ca3f51becd0d
David Blbulyan <blbulyandavbulyan@gmail.com>
2023-03-02 22:37:46 +0400

    task22 solved

---
+++ Seminar3/task22/Program.cs
@@ -0,0 +1,8 @@
+﻿Console.WriteLine("Введите N: ");
+int n = Convert.ToInt32(Console.ReadLine());
+for(int i = 1; i <= n; i++){
+  if(i != n){
+    Console.Write($"{i*i}, ");
+  }
+  else Console.WriteLine(Math.Pow(i, 2));
+}
