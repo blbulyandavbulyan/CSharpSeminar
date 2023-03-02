717694d140c47de1f9f3b270fca80561c2aa1b5f
David Blbulyan <blbulyandavbulyan@gmail.com>
2023-03-02 22:16:21 +0400

    added good user interface in task21

--- Seminar3/task21/Program.cs
+++ Seminar3/task21/Program.cs
@@ -6,9 +6,13 @@ int getInt(){
   return Convert.ToInt32(Console.ReadLine());
 }
 Console.WriteLine("Введите координаты точки A и B: ");
+Console.Write("Введите A_x: ");
 int x1 = getInt();
+Console.Write("Введите A_y: ");
 int y1 = getInt();
+Console.Write("Введите B_x: ");
 int x2 = getInt();
+Console.Write("Введите B_y: ");
 int y2 = getInt();
 double d = Math.Sqrt(Math.Pow(x1 - x2, 2) + Math.Pow(y1 - y2, 2));
 Console.WriteLine(d);
