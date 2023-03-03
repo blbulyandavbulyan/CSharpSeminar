1b242df710bac20b85e80b6b1d0370646d4809d5
David Blbulyan <blbulyandavbulyan@gmail.com>
2023-03-03 21:48:19 +0400

    task19 from homework for seminar3 was solved

---
+++ HomeWorkForSeminar3/task19/Program.cs
@@ -0,0 +1,24 @@
+﻿// Задача 19
+
+// Напишите программу, которая принимает на вход пятизначное число и проверяет, является ли оно палиндромом.
+
+// 14212 -> нет
+
+// 12821 -> да
+
+// 23432 -> да
+bool checkPolindrom(int number){
+  int count_digits = number.ToString().Length;//жулничество, перевёл в строку чтоб узнать сколько цифр в числе
+  int reversed_number = 0;
+  //в данном цикле счётчик i нужен для получения цифр с конца, а счётчик j цифр с начала
+  for(int i = number, power = count_digits - 1; i >= 1; i /= 10, power--){
+    int digit = i % 10;
+    Console.WriteLine($"digit: {digit}, power: {power}");
+    reversed_number += digit*(int)Math.Pow(10, power);
+  }
+  Console.WriteLine(reversed_number);
+  return number == reversed_number;
+}
+Console.Write("Введите число: ");
+int n = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine(checkPolindrom(n) ? "да" : "нет");
